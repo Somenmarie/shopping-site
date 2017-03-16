7ee8be6115a8822294f92090e5addb1bdd105853
marieotsuka <otsuka.m9521@gmail.com>
2017-03-16 13:11:26 +0000

    lesson9 up

---
+++ shopping.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("#catego").click(function(){
+    $(".catego_li").slideToggle();
+  });
+});
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
